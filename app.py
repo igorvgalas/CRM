1ab18d580b557b72a973a7fbdcdb391eb128a214
Ihor Halas <igorvgalas@gmail.com>
2023-04-04 00:22:27 +0200

    fix the bug in app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ recorder = Record(conn, client_list_done)
 recorder.record_clients()
 recorder.record_orders()
 
-with open("CRM/log_file.txt", "a+", encoding="utf-8") as file:
+with open("log_file.txt", "a+", encoding="utf-8") as file:
     file.seek(0)
     data = file.read(100)
     if len(data)>0:
